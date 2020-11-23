f2905f74f586ff071b8c5d626b9a493c545134c2
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-11-23 12:33:17 +0100

    Add files via upload

--- 一行代码系列/迷宫图案.py
+++ 一行代码系列/迷宫图案.py
@@ -9,10 +9,9 @@ software: PyCharm
 Description:
 """
 
-print(''.join(__import__('random').choice('\u2571\u2572') for i in range(50*24)))
+print('\n'.join([''.join([__import__('random').choice('\u2571\u2572') for i in range(80)]) for j in range(24)]))
 """
-在pycharm中无法实现，命令行中可以实现
 \u2571:代表符号/
 \u2572:代表符号\
-
+‘’.join()去掉了列表的括号
 """
