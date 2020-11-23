97a73ee48d6065296e1762edf734f45c830d72aa
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-11-23 12:24:54 +0100

    Add files via upload

---
+++ 一行代码系列/迷宫图案.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+"""
+author: 15025
+age: 26
+e-mail: 1502506285@qq.com
+time: 2020/11/1912:21
+software: PyCharm
+
+Description:
+"""
+
+print(''.join(__import__('random').choice('\u2571\u2572') for i in range(50*24)))
+"""
+在pycharm中无法实现，命令行中可以实现
+\u2571:代表符号/
+\u2572:代表符号\
+
+"""
